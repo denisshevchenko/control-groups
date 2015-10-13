fc71ddcb7f35c70cfb7e06d061eedf3abf909aee
Denis Shevchenko <shev.denis@gmail.com>
2015-10-13 20:18:06 +0000

    Adding of default.nix.

---
+++ default.nix
@@ -0,0 +1,34 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, aeson-pretty, attoparsec, base
+      , bytestring, containers, directory, fastcgi, filepath, process
+      , split, stdenv, utf8-string
+      }:
+      mkDerivation {
+        pname = "control-groups";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          aeson aeson-pretty attoparsec base bytestring containers directory
+          fastcgi filepath process split utf8-string
+        ];
+        homepage = "http://github.com/githubuser/control-groups#readme";
+        description = "Simple project template from stack";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
